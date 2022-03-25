aedcecce207e59672cb0c64ef38e8ee11555cc4f
verofoxy <veronia.mikhail@epitech.eu>
2022-03-25 12:19:02 +0100

    [add] technical test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { fromEvent } from 'rxjs';
+import { throttleTime, scan } from 'rxjs/operators';
 
 function App() {
+
+  function hello() {
+
+    fromEvent(document, 'click')
+      .pipe(
+        throttleTime(1000),
+        scan(count => count + 1, 0)
+
+      )
+      .subscribe(count => {
+        if (count % 3 === 0 && count % 5 === 0) {
+          console.log("FizzBuzz", count)
+        }
+        else if (count % 3 === 0) {
+          console.log("Fizz", count)
+        }
+        else if (count % 5 === 0) {
+          console.log("Buzz", count)
+        }
+        else if (count % 3 !== 0 && count % 5 !== 0) {
+          console.log(`Clickkkkked ${count} times`)
+        }
+      })
+
+  }
+
+  hello();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"
+          onClick={() => hello()} />
+
       </header>
     </div>
   );
