99e33b2acf6c46b9909faf105208be66a553cd6b
veronia <veronia@MacBook-Air-de-veronia.local>
2022-04-06 12:08:19 +0200

    [add] part 2 - step 0.5

---
+++ vue/part-2/my-first-vue/src/components/HomeLink.vue
@@ -0,0 +1,33 @@
+<script>
+import NavLink from "./components/NavLink.vue";
+
+export default {
+  name: "App",
+  components: {
+   NavLink
+  },
+  props : {
+    msg: {
+      type: String,
+      default: "Welcome to Your Vue.js App"
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <nav>
+    <NavLink url="/"> Home />
+    <NavLink url="/contact"> contact/>
+    </nav>
+    <p> Bienvenue sur notre page d' <HomeLink/></p>
+  </div>
+</template>
+
+<style>
+a {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
---
+++ vue/part-2/my-first-vue/src/components/NavLink.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: "NavLink",
+  propps: ["url", "text"],
+};
+</script>
+
+<template>
+  <a href="url"> {{ text }} </a>
+</template>
